a57543f428c330409e5d716549f6011d66a53cb6
Vladimir Zhevagin <vladimir.zhevagin@yandex.ru>
2021-01-23 18:27:26 +0300

    code add

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 5740;
+        int miles = ticket_price / 20;
+        System.out.println(miles);
+    }
+}

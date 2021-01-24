76ce7031e44a35a55dd0619e2de73af77ffc37a6
Vladimir Zhevagin <vladimir.zhevagin@yandex.ru>
2021-01-24 19:48:33 +0300

    Модификация

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 5740;
-        int miles = ticket_price / 20;
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
